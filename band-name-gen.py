55820de125c07967f3c01b5e097c302e680f0b5e
Ian Blomley <iblomley@gmail.com>
2023-01-25 16:10:48 +0000

    initial sync

---
+++ band-name-gen.py
@@ -0,0 +1,14 @@
+import time
+
+#1. greating
+print("Welcome to Banksy's band name generator v0.1 \n")
+#2. ask user what city they were born in
+city=input("What city were you born in?\n")
+#3. ask user for name of their pet
+pet=input("What is the name of your pet?\n")
+#4. combine both inputs to make their band name
+print("\nYour band name could be")
+time.sleep(2); print(".")
+time.sleep(2); print(".")
+time.sleep(2); print(".")
+print((pet) + " " + (city))
