1f3b389b6209867412c2050e08a4761cf2423797
Ian Blomley <iblomley@gmail.com>
2023-01-25 16:11:45 +0000

    no2

---
+++ examples.py
@@ -0,0 +1,15 @@
+# print("hello world")
+
+# nested functions
+# get users name with input, then use Len() to count the characters
+print(len(input("What is your name? ")))
+
+# variables
+
+
+name = "Jack"
+print (name)
+
+name = "Sarah"
+
+print (name)
