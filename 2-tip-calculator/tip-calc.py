995f7e8d4c1411dfa5a7c1ad2c76e254de18ae36
Ian Blomley <iblomley@gmail.com>
2023-02-24 15:55:37 +0000

    completed tip calculator

--- 2-tip-calculator/tip-calc.py
+++ 2-tip-calculator/tip-calc.py
@@ -1,2 +1,39 @@
-print("Welcome to the tip calculator.")
+print("\n\nWelcome to the tip calculator.")
+
+totalBill = input("What was the total bill? £")
+tipPercent = input("What % tip would you like to give? 10, 12, or 15 ")
+noPeople = input("How many people to split the bill? ")
+
+# print(type(totalBill))
+# print(type(tipPercent))
+# print(type(noPeople))
+# all start as string 
+
+#add a 1. to % to make for quicker maths formula 
+tipPercent2 = "1."+tipPercent
+# print(tipPercent2)
+
+#convert to int/float
+flt_totalBill = float(totalBill)
+flt_tipPercent = float(tipPercent2)
+int_noPeople = int(noPeople)
+
+#check conversion
+# print(type(flt_totalBill))
+# print(type(flt_tipPercent))
+# print(type(int_noPeople))
+
+totalPerPerson = (flt_totalBill / int_noPeople) * flt_tipPercent
+rndTotalPerPerson = round(totalPerPerson,2)
+rndTotalPerPerson = "{:.2f}".format(totalPerPerson)
+# print(type(rndTotalPerPerson))
+strPerPerson = str(rndTotalPerPerson)
+# print(type(strPerPerson))
+strPerPersonBill = "£"+strPerPerson
+print("Each person should pay: ",strPerPersonBill,"\n\n")
+# print("Each person should pay: £",round(totalPerPerson,2),"\n\n")
+
+
+
+
 
--- notes.py
+++ notes.py
@@ -42,7 +42,10 @@ print(type(variable_name))
 str(number_char)
 new_number_char = str(number_char)
 
+#####################
 # Day 2 - Exercise 1
+#####################
+
 
 # 🚨 Don't change the code below 👇
 two_digit_number = input("Type a two digit number: ")
@@ -59,3 +62,87 @@ num_digit_no2 = int(two_digit_number[1])
 # print(type(num_digit_no1))
 # print(type(num_digit_no2))
 print (num_digit_no1 + num_digit_no2)
+
+
+#####################
+# Day 2 - Exercise 2
+#####################
+
+PEMDAS (left to right)
+()
+**
+* /
++ -
+
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# print(type(height))
+# print(type(weight))
+flt_height = float(height)
+flt_weight = float(weight)
+# print(type(flt_height))
+# print (flt_height)
+flt_bmi = (flt_weight / (flt_height * flt_height))
+print (int(flt_bmi))
+
+
+# round numbers 
+print(round(8 / 3))
+# round by 2 decimal places
+print(round(8 / 3, 2))
+
+# when dividing, output always a floating point num
+
+# use // to remove decimals - doesnt round up
+print (8 // 3)
+
+# add 1 to a variable 'user-score' current value
+user-score += 1
+# can also use -+ /= *=
+
+# mix data types
+
+# f-string
+
+score = 0
+height = 1.8
+isWinning = True
+print (f"your score is {score} {height} {isWinning}")
+
+
+# format a number to give 2 decimal places in case of currency
+
+rndTotalPerPerson = "{:.2f}".format(totalPerPerson)
+
+#####################
+# Day 2 - Exercise 3
+#####################
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+int_age = int(age)
+#print(type(int_age))
+max_age = 90
+years_remaining = max_age - int_age
+#days
+days = (max_age - int_age) * 365
+#print(days)
+#weeks 
+weeks = years_remaining * 52
+#print(weeks)
+#months
+months = years_remaining * 12
+#print(months)
+print (f"You have {days} days, {weeks} weeks, and {months} months left.")
+
